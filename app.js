db50a98965ab3406b06342dbf7c010adaee73fd3
Your Name <20010964@st.phenikaa-uni.edu.vn>
2022-12-12 17:40:36 +0700

    gio hang + api

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const close = document.querySelector(".close");
-const open = document.querySelector(".ham");
-const menu = document.querySelector(".menu");
-close.addEventListener("click", () => {
-  menu.style.visibility = "hidden";
-});
-open.addEventListener("click", () => {
-  menu.style.visibility = "visible";
-});
+const items = document.getElementsByClassName("items")
+const url = 'https://fakestoreapi.com/products'
+
+async function getapishop() {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+  
+}
+getapishop().then(data =>{
+  const items = document.getElementsByClassName("items")
+  for(var i=0; i< 18 ;i++){
+    items[i].innerHTML=`
+              <div class="img"><img class="product-img" src="${data[i].image}" alt=""></div>
+              <div class="name">${data[i].category}</div>
+              <div class="price">${data[i].price}$</div>
+              <div class="info">${data[i].description}</div>
+              <div class="btnpay">
+                <button class="btnbuy">buy now</button>
+                <button class="btnaddcar">add cart</button>
+              </div>
+     `
+  }
+})
